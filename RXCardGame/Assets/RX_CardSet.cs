e369bbd979b4040255772709762d3904fecde984
lewis <xiaohao@lanou3g.com>
2015-08-07 10:07:10 +0800

    完成了游戏算法及出牌算法的测试

---
+++ RXCardGame/Assets/RX_CardSet.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssemblyCSharp
+{
+	/// <summary>
+	/// 11种牌型
+	/// </summary>
+	public enum RX_CARD_SET
+	{
+		RX_TYPE_DAN 		= 0,
+		RX_TYPE_DUI			= 1,
+		RX_TYPE_FEI_BUDAI	= 2,
+		RX_TYPE_FEI_DAI		= 3,
+		RX_TYPE_SHUN		= 4,
+		RX_TYPE_LIANDUI		= 5,
+		RX_TYPE_BOOM		= 6,
+		RX_TYPE_WANGZHA		= 7,
+		RX_TYPE_SIDAIER		= 8,
+		RX_TYPE_SAN_BUDAI	= 9,
+		RX_TYPE_SAN_DAI		= 10,
+	}
+
+	public class RX_CardSet
+	{
+		#region 属性设置
+		//牌列表
+		private List<RX_Card> 	card_list;
+		//牌类型
+		private RX_CARD_SET		card_type;
+		//牌大小
+		private RX_CARD_LEVEL	card_level;
+
+
+		/// <summary>
+		/// Gets or sets the list.
+		/// </summary>
+		/// <value>The list.</value>
+		public List<RX_Card> Lister{
+			set{ 
+				card_list = value;
+			}
+			get{ 
+				return card_list;
+			}
+		}
+		public RX_CARD_SET Typer{
+			set{ 
+				card_type = value;
+			}
+			get{ 
+				return card_type;
+			}
+		}
+		public RX_CARD_LEVEL Level{
+			set{ 
+				card_level = value;
+			}
+			get{ 
+				return card_level;
+			}
+		}
+		#endregion
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AssemblyCSharp.RX_CardSet"/> class.
+		/// </summary>
+		public RX_CardSet ()
+		{
+		}
+
+		public override string ToString ()
+		{
+			string str_card = "";
+
+			for (int i = 0; i < this.Lister.Count; i++) 
+			{
+				str_card += this.Lister[i].ToString() + " : ";	
+			}
+			
+			return string.Format ("[RX_CardSet: Lister={0}, Typer={1}, Level={2}]", str_card, Typer, Level);
+		}
+	}
+}
+
---
+++ RXCardGame/Assets/RX_Manager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+using System.Collections.Generic;
+
+public class RX_Manager : MonoBehaviour {
+
+
+	public Camera world_camera;
+	public Camera gui_camera;
+
+	//public prefab link
+	public Object prefab;
+
+	//public bottom link
+	public UISprite bottom_pool;
+	//public left	link
+	public UISprite left_pool;
+	//public right	link
+	public UISprite right_pool;
+
+	// Use this for initialization
+
+	private RX_SeatInfo bottom_seat;
+
+
+	public UILabel label;
+	void Start () 
+	{
+		this.Reshuffle ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PopSet()
+	{
+		RX_CardSet cardset = bottom_seat.PopCardSet ();
+
+		if (cardset == null) {
+			label.text = "出牌失败";
+			Debug.Log ("出牌失败");
+		} else {
+			label.text = "成功出牌" + cardset.ToString ();
+			Debug.Log ("成功出牌" + cardset.ToString ());
+		}
+	}
+
+	public void Reshuffle()
+	{
+		//shuffle the card
+		List<RX_Card> list = RX_CardManager.DefaultManager().Reshuffle ();
+
+
+		bottom_seat = new RX_SeatInfo(RX_SEAT_POSITION.RX_SEAT_BOTTOM,this.bottom_pool);
+		bottom_seat.CardList = list.GetRange(0,17);
+	}
+}
